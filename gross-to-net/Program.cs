fb2d33c7710662ed8bee28d7a40a8223d5846dbb
Cory Ball <cball@netchexonline.com>
2024-06-26 00:38:01 -0500

    add gross-to-net quickstart and devcontainer

---
+++ gross-to-net/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GrossToNet;
+
+class Program
+{
+    private static string InputFilePath => Path.Combine(AppContext.BaseDirectory, "input.json");
+
+    static void Main(string[] args)
+    {
+      // Your code goes here!
+      
+      throw new NotImplementedException();
+    }
+}
